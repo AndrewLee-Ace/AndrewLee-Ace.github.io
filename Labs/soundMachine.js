dc053fba13a90e2121c74271f516ee344205e8b2
Andrew Lee <acelee500@gmail.com>
2022-12-18 21:51:14 -0500

    Sound Machine Complete

---
+++ Labs/soundMachine.js
@@ -0,0 +1,26 @@
+var x = document.getElementById("horse");
+var y = document.getElementById("pig");
+var z = document.getElementById("dog");
+var w = document.getElementById("cat");
+var v = document.getElementById("cow");
+
+
+function playHorse(){
+x.play();
+}
+
+function playDog(){
+z.play();
+}
+
+function playCat(){
+w.play();
+}
+
+function playCow(){
+v.play();
+}
+
+function playPig(){
+y.play();
+}
--- Projects/animation.js
+++ Projects/animation.js
@@ -1,46 +1,94 @@
+// const boy = new Image();
+    const girl = new Image();
+    // const shark = new Image();
+
+function init() {
+    // boy.src="../boy.png";
+    girl.src="../girl.png";
+    //shark.src="../shark.png";
+    
+}
+
 function drawCanvas(){
     var canvas = document.getElementById("sandbox");
     var context = canvas.getContext("2d");
-    // context.fillStyle = 'lightgray';
-    // context.beginPath();
-    // context.arc(400, 450, 50, 0, Math.PI * 2);
-    // context.fill();
+   
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
+    //boy.onload = () => context.drawImage(boy,250, 0);
+    girl.onload = () => context.drawImage(girl,70,200);
+   
+    
 
-    function Circle(x,y,r,c){
-        this.x = x;
-        this.y = y;
-        this.r = r;
-        this.c = c;
+    context.beginPath();
+    context.fillStyle="black";
+    context.rect(110,150, 100, 50);
+    context.stroke();
+    
 
-        this.dx;
-        this.dy;
+    context.fillText("What a good day", 120, 170);
+    context.fillText("at the beach!", 120, 180);
 
-        this.draw = function() {
-            context.beginPath();
-            context.fillStyle = this.c;
-            context.arc(this.x, this.y, this.r,  0, Math.PI * 2);
-            context.fill();
-        }
-    }
+    
+    // function Circle(x,y,r,c){
+    //     this.x = x;
+    //     this.y = y;
+    //     this.r = r;
+    //     this.c = c;
 
-    const balls = [];
+    //     this.dx = (Math.random() * 4) + 1;
+    //     this.dx *= Math.floor(Math.random() * 2) == 1 ? 1 : -1
+    //     this.dy = (Math.random() * 4) + 1;
+    //     this.dy *= Math.floor(Math.random() * 2) == 1 ? 1 : -1
 
-    for (let i = 0; i < 20; i++) {
-        let r = Math.floor(Math.random() * 30) + 15 //random radius between 15 and 30
-        let x = Math.random() * (canvas.width - r * 2) + r; //minus diamter so balls dont spawn in wall
-        let y =  Math.random() * (canvas.height - r * 2) + r;
-        let c = 'red';
-        balls.push(new Circle(x, y, r, c))
-    }
+    //     this.draw = function() {
+    //         context.beginPath();
+    //         context.fillStyle = this.c;
+    //         context.arc(this.x, this.y, this.r,  0, Math.PI * 2);
+    //         context.fill();
+    //     }
 
-    function Update(){
-        for (let i = 0; i <balls.length; i++){
-            balls[i].draw();
-        }
-    }
-    Update();
+    //     this.animate = function () {
+    //         this.x += this.dx;
+    //         this.y += this.dy;
+
+    //         if (this.x + this.r > canvas.width || this.x - this.r < 0){
+    //             this.dx = -this.dx
+    //         }
+
+    //         if (this.y + this.r > canvas.height || this.y - this.r < 0){
+    //             this.dy = -this.dy
+    //         }
+
+    //         this.draw();
+    //     }
+
+        
+    // }
+
+    // const balls = [];
+
+    // for (let i = 0; i < 20; i++) {
+    //     let r = Math.floor(Math.random() * 30) + 15 //random radius between 15 and 30
+    //     let x = Math.random() * (canvas.width - r * 2) + r; //minus diamter so balls dont spawn in wall
+    //     let y =  Math.random() * (canvas.height - r * 2) + r;
+    //     let c = 'red';
+    //     balls.push(new Circle(x, y, r, c))
+    // }
+
+    // function Update(){
+    //     context.clearRect(0, 0, canvas.width, canvas.height);
+    //     for (let i = 0; i <balls.length; i++){
+    //         balls[i].animate();
+    //     }
+
+    //     requestAnimationFrame(Update);
+    // }
+
+    
+    // Update();
      
 }
 
-document.addEventListener("DOMContentLoaded", drawCanvas);
+init();
+//document.addEventListener("DOMContentLoaded", drawCanvas);
